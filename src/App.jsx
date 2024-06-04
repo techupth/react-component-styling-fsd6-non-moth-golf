1a7e7acd951c84dea88e182eb7e35f8f863eee44
fossi13 <fossi13@users.noreply.github.com>
2024-06-04 18:01:31 +0700

    Fix: add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
 import "./App.css";
+import Button from "./components/Button";
+import Alert from "./components/Alert";
 
 function App() {
   return (
     <div className="App">
-      <div className="button-components-section">
-        {/* Render ตัว Button 2 แบบ */}
-      </div>
-      <hr />
-      <div className="alert-components-section">
-        {/* Render ตัว Alert 4 แบบ */}
+      <h1>Buttons & Alert</h1>
+      <div className="container">
+        <div className="button-components-section">
+          <div className="btn">
+            <span>Primary</span>
+            <span>
+              <Button type="primary" />
+            </span>
+          </div>
+          <div className="btn">
+            <span>Secondary</span>
+            <span>
+              <Button type="secondary" />
+            </span>
+          </div>
+          <div className="btn">
+            <span>Error</span>
+            <span>
+              <Button type="error" />
+            </span>
+          </div>
+          <div className="btn">
+            <span>Success</span>
+            <span>
+              <Button type="success" />
+            </span>
+          </div>
+        </div>
+        <hr />
+        <div className="alert-components-section">
+          <Alert type="error"/>
+          <Alert type="warning"/>
+          <Alert type="info"/>
+          <Alert type="success"/>
+        </div>
       </div>
     </div>
   );
--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,100 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import frown from "../assets/frown.svg";
+import warn from "../assets/alert-triangle.svg";
+import circle from "../assets/alert-circle.svg";
+import check from "../assets/check-circle.svg";
 
-function Alert() {}
+function Alert(props) {
+  if (props.type === "error") {
+    return (
+      <button
+        css={css`
+          background-color: #f9c8c8;
+          color: #444444;
+          width: 650px;
+          height: 76px;
+          border-radius: 10px;
+          padding: 0px 16px 0px 16px;
+          display: flex;
+          border: none;
+          align-items: center;
+          font-weight: 700;
+          font-family: Kanit;
+          font-size: 20px;
+        `}
+      >
+        <img src={frown} />
+        This is error alert box
+      </button>
+    );
+  } else if (props.type === "warning") {
+    return (
+      <button
+        css={css`
+          background-color: #f9d9c8;
+          color: #444444;
+          width: 650px;
+          height: 76px;
+          border-radius: 10px;
+          padding: 0px 16px 0px 16px;
+          display: flex;
+          border: none;
+          align-items: center;
+          font-weight: 700;
+          font-family: Kanit;
+          font-size: 20px;
+        `}
+      >
+        <img src={warn} />
+        This is warning alert box
+      </button>
+    );
+  } else if (props.type === "info") {
+    return (
+      <button
+        css={css`
+          background-color: #f9ebc8;
+          color: #444444;
+          width: 650px;
+          height: 76px;
+          border-radius: 10px;
+          padding: 0px 16px 0px 16px;
+          display: flex;
+          border: none;
+          align-items: center;
+          font-weight: 700;
+          font-family: Kanit;
+          font-size: 20px;
+        `}
+      >
+        <img src={circle} />
+        This is info alert box
+      </button>
+    );
+  } else if (props.type === "success") {
+    return (
+      <button
+        css={css`
+          background-color: #cef7cd;
+          color: #444444;
+          width: 650px;
+          height: 76px;
+          border-radius: 10px;
+          padding: 0px 16px 0px 16px;
+          display: flex;
+          border: none;
+          align-items: center;
+          font-weight: 700;
+          font-family: Kanit;
+          font-size: 20px;
+        `}
+      >
+        <img src={check} />
+        This is success alert box
+      </button>
+    );
+  }
+}
+
+export default Alert;
